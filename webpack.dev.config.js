0828dc8ae330f6dff8691f77b9707aee1a9273c7
Al <alexseier12345@gmail..com>
2021-11-23 10:06:28 +0300

    create dev config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   devServer: {
@@ -8,34 +7,22 @@ module.exports = {
     historyApiFallback: true,
     hot: true,
   },
-  entry: {
-    main: ["@babel/polyfill", "./src/public/index.ts"],
-  },
+  entry: path.resolve(__dirname, "./src/index.ts"),
   output: {
-    path: path.join(__dirname, "dist/public"),
+    path: path.join(__dirname, "dist"),
     publicPath: "/",
-    filename: "js/[name].js",
+    filename: "bundle.js",
   },
-  target: "web",
-  devtool: "#source-map",
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.ts$/,
+        loader: "ts-loader",
         exclude: /node_modules/,
-        loader: "babel-loader",
       },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.(less|css)$/,
+        use: ["style-loader', 'css-loader', 'postcss-loader', 'sass-loader"]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
@@ -48,26 +35,8 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/public/index.html",
-      filename: "index.html",
-      excludeChunks: ["server"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/public/catalog.html",
-      filename: "catalog.html",
-      excludeChunks: ["server"],
-    }),
-    new HtmlWebpackPlugin({
-      template: "src/public/registration.html",
-      filename: "registration.html",
-      excludeChunks: ["server"],
-    }),
-    new CopyPlugin([
-      {
-        from: "src/public/img",
-        to: "img/[name].[ext]",
-        toType: "template",
-      },
-    ]),
+          template: path.resolve(__dirname, "./src/index.html")
+        }
+    ),
   ],
 };
