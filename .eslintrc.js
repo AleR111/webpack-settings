bd16e5f547c31883a0c2da4081c6c686008b3865
Al <alexseier12345@gmail..com>
2021-11-23 16:26:40 +0300

    add eslintignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
         "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended"
     ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
         },
-        "ecmaVersion": 13,
-        "sourceType": "module"
+        ecmaVersion: 13,
+        sourceType: "module"
     },
-    "plugins": [
+    plugins: [
         "react",
         "@typescript-eslint"
     ],
-    "rules": {
+    rules: {
     }
 };
--- webpack/general-settings.js
+++ webpack/general-settings.js
@@ -2,38 +2,35 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 export const generalSettings = {
-    paramsEntryOutput: {
-        entry: ["@babel/polyfill", "./src/index.ts"],
-        output: {
-            // eslint-disable-next-line no-undef
-            path: path.join(__dirname, "dist"),
-            filename: "bundle.js",
-        },
+  paramsEntryOutput: {
+    entry: ["@babel/polyfill", "./src/index.ts"],
+      output: {
+        path: path.join(__dirname, "dist"),
+        filename: "bundle.js",
+      },
     },
     moduleRules: [
-        {
-            test: /\.jsx?$/,
-            loader: "babel-loader",
-            exclude: /node_modules/,
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "file-loader",
+        options: {
+          name: "img/[name].[ext]",
         },
-        {
-            test: /\.ts$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.(png|jpg|svg|gif)$/,
-            loader: "file-loader",
-            options: {
-                name: "img/[name].[ext]",
-            },
-        },
+      },
     ],
     plugins: [
-        new HtmlWebpackPlugin({
-            // eslint-disable-next-line no-undef
-                template: path.resolve(__dirname, "./src/index.html")
-            }
-        ),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "./src/index.html")
+      }),
     ],
 }
