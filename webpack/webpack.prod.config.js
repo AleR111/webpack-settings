1f4fc3e3dedd88418a704ca9079f4b84b9b7b62d
Al <alexseier12345@gmail..com>
2021-11-26 11:03:56 +0300

    add minimizer for css

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -1,13 +1,14 @@
 const { merge } = require("webpack-merge");
 const baseWebpackConfig = require("./webpack.base.config");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const styleRules = require("./utils/styleRules");
 
 module.exports = merge(baseWebpackConfig, {
   optimization: {
     minimize: true,
-    minimizer: [new TerserPlugin()],
+    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
   },
   module: {
     rules: [...styleRules("prod")],
