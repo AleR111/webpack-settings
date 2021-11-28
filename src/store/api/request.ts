94a4d8e79cec957f6cae07c4ded6160ce060b7fb
Alexsei <alexseier12345@gmail.com>
2021-11-28 20:01:05 +0300

    update axios wrapper

--- src/store/api/request.ts
+++ src/store/api/request.ts
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 class Request {
-  get = (url: string, config?: object): Promise<any> => {
+  async get(url: string, config?: object): Promise<any> {
     return axios.get(url, config);
-  };
-
-  post = (utl: string, body: object, config?: object): Promise<string> => {
-    return axios.post(utl, body, config);
-  };
-
-  delete = (url: string, config?: object): Promise<string> => {
-      return axios.delete(url, config)
   }
 
-  put = (utl: string, body: object, config?: object): Promise<string> => {
-      return axios.put(utl, body, config);
-  };
+  async post(utl: string, body: object, config?: object): Promise<string> {
+    return axios.post(utl, body, config);
+  }
 
-    patch = (utl: string, body: object, config?: object): Promise<string> => {
-        return axios.patch(utl, body, config);
-    };
+  async delete(url: string, config?: object): Promise<string> {
+    return axios.delete(url, config);
+  }
+
+  async put(utl: string, body: object, config?: object): Promise<string> {
+    return axios.put(utl, body, config);
+  }
+
+  async patch(utl: string, body: object, config?: object): Promise<string> {
+    return axios.patch(utl, body, config);
+  }
 }
 
 export const requests = new Request();
