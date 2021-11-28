dc624c94827d8824d8a4528989d5c000e9f14b3e
Al <alexseier12345@gmail..com>
2021-11-28 16:33:07 +0300

    create axios wrapper

---
+++ src/api/request.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+class Request {
+  get = (url: string, config?: object): Promise<any> => {
+    return axios.get(url, config);
+  };
+
+  post = (utl: string, body: object, config?: object): Promise<string> => {
+    return axios.post(utl, body, config);
+  };
+
+  delete = (url: string, config?: object): Promise<string> => {
+      return axios.delete(url, config)
+  }
+
+  put = (utl: string, body: object, config?: object): Promise<string> => {
+      return axios.put(utl, body, config);
+  };
+
+    patch = (utl: string, body: object, config?: object): Promise<string> => {
+        return axios.patch(utl, body, config);
+    };
+}
+
+export const request = new Request();
