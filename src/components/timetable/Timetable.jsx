10de9ffb6e743471d380496485a3bf9269971001
Alexsei <alexseier12345@gmail.com>
2021-12-09 12:48:39 +0300

    refactor

--- src/components/timetable/Timetable.jsx
+++ src/components/timetable/Timetable.jsx
@@ -1,47 +1,58 @@
-import React, { useEffect, useRef } from "react";
+import React, { useCallback, useEffect } from "react";
 import * as d3 from "d3";
 
-const renderSvg = () => {
-  const zoom = d3.zoom().scaleExtent([1, 2000]).on("zoom", zoomed);
-
-  const svg = d3
-    .select("#box")
-    .append("svg")
-    .attr("width", '100%')
-    .attr("height", '100vh');
-
-  const gDot = svg
-    .append("g")
-    .attr("fill", "none")
-    .attr("stroke-linecap", "round")
-    .attr('fill', '#ece62473')
-
-  svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
+const getCoordinatesData = () => {
+  const dataArr = [];
 
   for (let i = 0; i < 100; i++) {
-
-    for(let j = 0; j < 100; j++) {
-      gDot
-      .append("rect")
-      .attr("width", 20)
-      .attr("height", 20)
-      .attr('x', j*20)
-      .attr('id', `${i}${j}`)
-      .attr('y', i*20)
-      .attr("stroke", "#000");
+    for (let j = 0; j < 100; j++) {
+      const data = { x: j * 20, y: i * 20, id: `${i}${j}`, width: 20, height: 20 };
+      dataArr.push(data);
     }
-    
   }
 
-  function zoomed({ transform }) {
-    gDot.attr("transform", transform).attr("stroke-width", 0.1 / transform.k);
-  }
+  return dataArr;
 };
 
 export const Timetable = () => {
-  useEffect(() => {
-    renderSvg();
+  const coordinatesData = getCoordinatesData();
+
+  const renderCoordinates = useCallback(() => {
+    const zoomed = ({ transform }) => {
+      d3.select("#coordinates")
+        .attr("transform", transform)
+        .attr("stroke-width", 0.1 / transform.k);
+    };
+
+    const zoom = d3.zoom().scaleExtent([1, 200]).on("zoom", zoomed);
+    const svg = d3.select("#timetable");
+    svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
   }, []);
 
-  return <div id="box" onClick={(e) => console.log(e.target.id)}></div>;
+  useEffect(() => {
+    renderCoordinates();
+  }, [renderCoordinates]);
+
+  return (
+    <div id="box">
+      <svg id="timetable" width="100%" height="100vh">
+        <g id="coordinates" fill="#ece62473">
+          {coordinatesData.map((el, index) => {
+            return (
+              <rect
+                key={index}
+                x={el.x}
+                y={el.y}
+                id={el.id}
+                width={el.width}
+                height={el.height}
+                stroke="#000"
+                onClick={(e) => console.log(e.target.id)}
+              />
+            );
+          })}
+        </g>
+      </svg>
+    </div>
+  );
 };
