439f242b79d0b8aee424554cfb09031b1de38f05
Alexsei <alexseier12345@gmail.com>
2021-12-20 10:04:40 +0300

    done tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styles from "./app.module.less";
-import { AddButton, SettingsButton, DeleteButton, CloseButton, ApplyButton, CancelButton } from "./components/ui-components";
+import {
+  AddButton,
+  SettingsButton,
+  DeleteButton,
+  CloseButton,
+  ApplyButton,
+  CancelButton,
+  Tabs,
+} from "./components/ui-components";
 // import { Timetable } from "./components";
 
 export const App: React.FC = () => {
@@ -12,6 +20,15 @@ export const App: React.FC = () => {
       <CloseButton />
       <ApplyButton />
       <CancelButton />
+      <div style={{ width: "400px" }}>
+        <Tabs
+          oneName="one"
+          twoName="two"
+          onePanel="1 panel"
+          twoPanel="2 panel"
+        />
+      </div>
+
       {/* <Timetable /> */}
     </div>
   );
---
+++ src/components/ui-components/tabs/Tabs.jsx
@@ -0,0 +1,51 @@
+import * as React from "react";
+import { Box, Tab, Tabs as TabsUI, styled } from "@mui/material";
+import { TabContext, TabPanel } from "@mui/lab";
+import "./tabs.module.less";
+
+const StyledTabs = styled((props) => <TabsUI {...props} />)({
+  minHeight: "unset",
+  "& .MuiTabs-indicator": {
+    display: "none",
+  },
+  "& .MuiTabs-flexContainer": {
+    padding: "2px",
+    backgroundColor: "#E8E8E8",
+  },
+});
+
+const StyledTab = styled((props) => <Tab {...props} />)({
+  minHeight: "unset",
+  textTransform: "none",
+  maxWidth: "unset",
+  minWidth: "unset",
+  padding: "8px 0",
+  flexGrow: 1,
+  color: "#8F8F8F",
+  borderRadius: "2px",
+  "&.Mui-selected": {
+    color: "#fff",
+    backgroundColor: "#FF630E",
+  }
+});
+
+export const Tabs = ({ oneName, twoName, onePanel, twoPanel }) => {
+  const [value, setValue] = React.useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <TabContext value={value}>
+      <Box>
+        <StyledTabs value={value} onChange={handleChange}>
+          <StyledTab label={oneName} value="1" />
+          <StyledTab label={twoName} value="2" />
+        </StyledTabs>
+      </Box>
+      <TabPanel value="1">{onePanel}</TabPanel>
+      <TabPanel value="2">{twoPanel}</TabPanel>
+    </TabContext>
+  );
+};
